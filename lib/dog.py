e6cee8e372de412ee43bc3613afb6e3d7f1c2475
NillsLopez <yakusaz17@gmail.com>
2024-09-22 19:52:30 -0400

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
-class Dog:
-    pass
+class Dog:  
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name} is born!")
+
+    def bark(self):
+        print("Woof!")      
+        
+    def showing_self(self):
+        return self 
+        
+#fido = Dog("Fido")
+
+#snoopy = Dog("Snoopy")
+
+#fido.showing_self()
